8ea84005093cbc683361b9aaf5098d114d3cb02b
Linwoo1012 <sittipol1012@gmail.com>
2023-08-12 04:18:56 +0700

    test io

--- timing.c
+++ timing.c
@@ -7,16 +7,17 @@ int main(){
 
     //run io
     start = clock();
-    for(long i = 0; i < 100; i++){
-        printf(".");
+    for(long i = 0; i < 1000000; i++){
+        printf("%d/n", i);
     }
     end = clock();
     printf("\nTime(I/O)\t: %lf s\n", (double)(end - start)/CLOCKS_PER_SEC);
 
     //dont run io
+    int a = 0;
     start2 = clock();
-    for(long i = 0; i < 100; i++){
-        ;
+    for(long i = 0; i < 1000000; i++){
+        a++;
     }
     end2 = clock();
     printf("Time(no I/O)\t: %lf s\n", (double)(end2 - start2)/CLOCKS_PER_SEC);
