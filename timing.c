954426d430f01834fd9bf3fc54d596dc46f155f2
Linwoo1012 <sittipol1012@gmail.com>
2023-08-20 00:16:27 +0700

    update run i/o
    change run i/o output and move it to function

--- timing.c
+++ timing.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <time.h>
 
+void run_io(){
+    for(long i = 0; i < 100000; i++){
+        printf("");
+    }
+}
+
+void dont_run_io(){
+    int a = 0;
+    for(long i = 0; i < 100000; i++){
+        a++;
+    }
+}
+
 int main(){
     clock_t start, end;
     clock_t start2, end2;
 
     //run io
     start = clock();
-    for(long i = 0; i < 100000; i++){
-        printf(" ");
-    }
+    run_io();
     end = clock();
     
     //dont run io
-    int a = 0;
     start2 = clock();
-    for(long i = 0; i < 100000; i++){
-        a++;
-    }
+    dont_run_io();
     end2 = clock();
     
     printf("\nTime(I/O)\t: %lf s\n", (double)(end - start)/CLOCKS_PER_SEC);
